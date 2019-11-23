60ede97157ae57e992db36927061c86fe2beeac1
Gray Marchiori <mo-g@users.noreply.github.com>
2019-11-23 03:59:53 +0000

    Changed variable names for readability, added minor comments.

--- dial_to_usb.ino
+++ dial_to_usb.ino
@@ -2,35 +2,35 @@
 
 #define counterButton   3
 #define openButton      4
-byte count = 0;
-bool lastIsOpen = true;
+byte clickCounter = 0;
+bool lastClickStillOpen = true;
 
 void setup()
 {
-  pinMode(counterButton, INPUT_PULLUP);
-  pinMode(openButton, INPUT_PULLUP);
+  pinMode(clickSwitch, INPUT_PULLUP);
+  pinMode(atRestSwitch, INPUT_PULLUP);
   Keyboard.begin();
 }
 
 void loop()
 {
-  if (digitalRead(openButton) == LOW) {
-    if (digitalRead(counterButton) == LOW) {
-      if (!lastIsOpen) {
-        lastIsOpen = true;
-        count++;
+  if (digitalRead(atRestSwitch) == LOW) {
+    if (digitalRead(clickSwitch) == LOW) {
+      if (!lastClickStillOpen) { // Ensure each click is finished before we count the next.
+        lastClickStillOpen = true;
+        clickCounter++;
         delay(10);
       }
     }
-    else {
-      lastIsOpen = false;
+    else { // When click ends, permit the recording of the next click.
+      lastClickStillOpen = false;
     }
   }
-  else if (count > 0) {
-    if (count == 10) {
-      count = 0;
+  else if (clickCounter > 0) {
+    if (clickCounter == 10) { // Transpose 10 clicks to character 0.
+      clickCounter = 0;
     }
-    Keyboard.print(str(count));
-    count = 0;
+    Keyboard.print(str(clickCounter)); // Send character to USB Host.
+    clickCounter = 0; // Blank clickCounter for next number dial.
   }
 }
